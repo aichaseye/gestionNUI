e0c0f07b95c8309d51d03a05e986d43b74e2bbe0
aicha seye <seyeaichaseye@gmail.com>
2022-06-29 17:30:47 +0200

    initialisation

--- src/main/java/sn/seye/gesmat/mefpai/security/AuthoritiesConstants.java
+++ src/main/java/sn/seye/gesmat/mefpai/security/AuthoritiesConstants.java
@@ -11,5 +11,16 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String PROVISEUR = "ROLE_PROVISEUR";
+    public static final String DIRECTEUR = "ROLE_DIRECTEUR";
+    public static final String BEFPA = "ROLE_BFPA";
+    public static final String CHEF_TRAVEAUX = "ROLE_CHEF_TRAVAUX";
+    public static final String COMPTABLE_MATIERE = "ROLE_COMPTABLE_MATIERE";
+    public static final String APPRENANT = "ROLE_APPRENANT";
+
+    public static final String CHEF_TRAVAUX = null;
+
+    public static final String BFPA = null;
+
     private AuthoritiesConstants() {}
 }
